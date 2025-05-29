fa262b73428727ba20658f8b4d9238cba209780b
oligeon-dev <oligeon.dev@gmail.com>
2025-05-29 14:27:53 +0900

    maskable辞めてみる

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,6 @@ function makeManifest(appName: string) {
         src: `${baseUrl}/152.png`,
         sizes: '152x152',
         type: 'image/png',
-        purpose: 'maskable',
       },
       { src: `${baseUrl}/192.png`, sizes: '192x192', type: 'image/png' },
       { src: `${baseUrl}/512.png`, sizes: '512x512', type: 'image/png' },
