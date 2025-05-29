5ddd4ae1f6d9859fe74edb086aa5c5c888bf9a36
oligeon-dev <oligeon.dev@gmail.com>
2025-05-29 11:27:10 +0900

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,15 @@ function makeManifest(appName: string) {
     display: 'standalone',
     start_url: baseUrl,
     icons: [
+      { src: `${baseUrl}/48.png`, sizes: '48x48', type: 'image/png' },
+      { src: `${baseUrl}/72.png`, sizes: '72x72', type: 'image/png' },
+      { src: `${baseUrl}/96.png`, sizes: '96x96', type: 'image/png' },
+      { src: `${baseUrl}/128.png`, sizes: '128x128', type: 'image/png' },
+      { src: `${baseUrl}/142.png`, sizes: '142x142', type: 'image/png' },
+      { src: `${baseUrl}/152.png`, sizes: '152x152', type: 'image/png' },
       { src: `${baseUrl}/192.png`, sizes: '192x192', type: 'image/png' },
       { src: `${baseUrl}/512.png`, sizes: '512x512', type: 'image/png' },
-      { src: `${baseUrl}/512.png`, sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+      // { src: `${baseUrl}/512.png`, sizes: '512x512', type: 'image/png', purpose: 'maskable' },
     ],
   };
 }
