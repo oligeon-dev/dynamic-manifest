b9f55044f37c0d5f9eb31163714a5a6697d91839
oligeon-dev <oligeon.dev@gmail.com>
2025-05-29 11:31:36 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,12 @@ function makeManifest(appName: string) {
       { src: `${baseUrl}/48.png`, sizes: '48x48', type: 'image/png' },
       { src: `${baseUrl}/72.png`, sizes: '72x72', type: 'image/png' },
       { src: `${baseUrl}/96.png`, sizes: '96x96', type: 'image/png' },
-      { src: `${baseUrl}/128.png`, sizes: '128x128', type: 'image/png' },
+      {
+        src: `${baseUrl}/128.png`,
+        sizes: '128x128',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
       { src: `${baseUrl}/142.png`, sizes: '142x142', type: 'image/png' },
       { src: `${baseUrl}/152.png`, sizes: '152x152', type: 'image/png' },
       { src: `${baseUrl}/192.png`, sizes: '192x192', type: 'image/png' },
